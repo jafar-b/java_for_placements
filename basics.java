ad866a0d942fda8e351fc14f3876c9edf99e3e82
jafar-b <beldarjafar.com>
2023-12-29 08:17:59 +0530

    reverse of a number, palindrome check

--- basics.java
+++ basics.java
@@ -1,8 +1,64 @@
-public class basics{
+// //reverse of a number
+// public class basics{
 
-public static void main(String Args[])
-{
+// public static void main(String Args[])
+// {
+// int a=12345;
+// getRev(a);
+// }
 
-    
-}
-}
+// static void getRev(int n){
+// if(n==0)return ;
+// int digit=n%10;
+// System.out.print(digit);
+// getRev(n/10);
+// }
+// }
+  
+//OR using while loop reverse of a number
+
+
+// public class basics{
+
+// public static void main(String Args[])
+// {
+
+//   int a=123456789;
+// while(a!=0){
+//   int digit=a%10;
+//   System.out.print(digit);
+//   a=a/10;
+
+// }
+// }
+
+
+// }
+  
+
+
+// //Palindrome check
+// public class basics{
+// public static void main(String Args[])
+// {
+// int a=12321;
+// System.out.println(check(a));}
+
+// static boolean check(int a){
+//   int rev=0;
+//   int temp=a;
+//   while(temp!=0){
+//     int digit=temp%10;
+//     rev= rev*10+digit;
+//     temp/=10;
+  
+//   }
+//   if(rev==a)return true;
+
+// return false;
+// }
+// }
+  
+
+
+
