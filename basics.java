e2d0a7ffede0ec6b10e3663ef547a3321413bc3a
jafar-b <beldarjafar.com>
2023-12-30 13:42:05 +0530

    armstrong number check

--- basics.java
+++ basics.java
@@ -62,3 +62,38 @@
 
 
 
+// //armstrong number
+// public class basics{
+
+
+//     public static void main(String Args[]){
+//         int a=371;
+//         check(a);
+//     }
+
+// static void check(int a){
+// int sum=0;
+// int temp=a;
+// while(temp!=0){
+// int digit=temp%10;
+// sum=sum+((int)(Math.pow(digit,order(a))));
+// temp=temp/10;
+// }
+// if(sum==a){System.out.println("Armstrong");}
+// else{System.out.println("Not an armstrong as the sum is "+sum );}
+// }
+
+
+
+// static int order(int a){
+// int order=0;
+// while(a!=0){
+//     order++;
+//     a=a/10;
+// }
+//     return order;
+// }
+// }
+
+
+
