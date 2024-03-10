dd4d8a983e1d0af0868bd1615426dbcfa4e3b312
jafar <beldarjafar@gmail.com>
2024-03-10 14:32:25 +0530

    subarray with the given sum

--- basics.java
+++ basics.java
@@ -1200,5 +1200,32 @@
 // }
 
 
+// //subarray with the given sum
+// public class basics {
+//     public static void main(String Args[]) {
 
-// public class basics{public static void main(String Args[]){  } }
+//         int arr[] = {  15, 2, 4, 8, 9, 5, 10, 23};
+//         int sum = 23;
+//         find(arr,sum);
+//     }
+
+//     static void find(int[] arr, int sum) {
+       
+//         for (int i = 0; i < arr.length; i++) {
+//             int currsum = arr[i];
+//             if (currsum == sum) {
+//                 System.out.println("Sum found at index: " + i);
+//             }
+//             if (currsum < sum) {
+//                 for (int j = i + 1; j < arr.length; j++) {
+//                     currsum = currsum + arr[j];
+//                     if (currsum == sum) {
+//     System.out.println("Sum found between indexes: "+ i + " and "+j);
+// }
+//                 }
+//             }
+         
+
+//         }
+// }
+// }
