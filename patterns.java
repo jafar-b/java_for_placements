55ac490af2afc6c359abb01ebf8710bb84d827c4
jafar <beldarjafar@gmail.com>
2024-03-21 21:25:55 +0530

    star pattern revision

--- patterns.java
+++ patterns.java
@@ -487,23 +487,52 @@
 //     }
 // };
 
-
-
-
 // 1*2*3*4
 // 5*6*7*8
 // 9*10*11*12
 // 13*14*15*16*
 
+// public class patterns {
+//     public static void main(String Args[]) {
+//         int n = 4;
+//         int count = 1;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 System.out.print(count++ + "*");
+//             }
+//     System.out.println();
+//  }
+//     }
+// };
+
+//      *
+//     ** 
+//    *** 
+//  ***** 
+// ******
+
+// public class patterns {
+//     public static void main(String Args[]) {
+//         int n = 5;
+//         for (int i = 0; i < n; i++) {
+//             System.out.print("|");
+//             for (int j = n-1; j >=i; j--) {
+//                 System.out.print(" ");
+//             }
+//             for (int j = 0; j <=i; j++) {
+//                System.out.print("*"); 
+//             }
+//             System.out.println();
+//         }
+//     }
+// };
+
 public class patterns {
     public static void main(String Args[]) {
-        int n = 4;
-        int count = 1;
+        int n = 5;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(count++ + "*");
-            }
-    System.out.println();
- }
+            System.out.print("|");
+
+        }
     }
 };
